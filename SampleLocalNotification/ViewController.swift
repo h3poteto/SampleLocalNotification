6361ba042a8db0c0a2de0fd200cea96efffc2bd5
h3poteto <h3.poteto@gmail.com>
2015-02-23 01:59:05 +0900

    set local notification

--- SampleLocalNotification/ViewController.swift
+++ SampleLocalNotification/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var setLocalNotificationButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setLocalNotificationButton.addTarget(self, action: "setLocalNotification", forControlEvents: UIControlEvents.TouchDown)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setLocalNotification() {
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        var localNotification = UILocalNotification()
+        localNotification.fireDate = NSDate(timeIntervalSinceNow: 60)
+        localNotification.alertBody = "localNotification"
+        localNotification.timeZone = NSTimeZone(forSecondsFromGMT: 9 * 60 * 60)
+        localNotification.soundName = UILocalNotificationDefaultSoundName
+        localNotification.alertAction = "OPEN"
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+    }
 
 }
 
